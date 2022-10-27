ef3b472159adbb3646572a07041698f54954d6d4
John Everton <johneverton44@gmail.com>
2022-10-27 19:06:36 -0300

    feature: crate sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { Header } from "./components/Header";
+import { Header } from './components/Header';
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 export function App() {
   return (
     <>
       <Header />
-      <h1>Hello world</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          posts
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1563120425-aacf863ea7b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/23002870?v=4"
+          alt="John Everton"
+        />
+        <strong>John Everton</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
