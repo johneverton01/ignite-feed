7d651c70d3447b4638dca6b332c9931e39c55028
John Everton <johneverton44@gmail.com>
2022-10-27 08:49:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+import './global.css';
+export function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+// Default export vs Named Exports
